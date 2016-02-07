6e9efa243b321ed9bebfd65511d9e321a97d34b2
Félix Saparelli <me@passcod.name>
2016-02-07 15:20:05 +1300

    Remove all bin deps apart from glob

--- bin/quenya.js
+++ bin/quenya.js
@@ -7,36 +7,31 @@
 // JSON objects to STDOUT, one per line. No further processing is done, and
 // as with Quenya, order is not guaranteed.
 
-const argv = require('minimist')(process.argv.slice(2), {
-  alias: { help: 'h' }
-})
-
-if (argv._.length === 0 || argv.help) {
-  console.error(
-    'quenya: Rustdoc-inspired code documentation\n' +
-    '\n' +
-    'Usage:\n' +
-    ' quenya [options] <files or quoted globs...>\n' +
-    '\n' +
-    'Options:\n' +
-    '  -h, --help             display this help and exit\n'
-  )
+const argv = process.argv.slice(2)
+if (argv.length === 0) {
+  console.error('Usage: quenya [options] <files or quoted globs...>')
   process.exit()
 }
 
-const glob = require('glob-promise')
-const map = require('through2-map').obj
-const path = require('path')
+function glob (pattern) {
+  return new Promise((resolve, reject) => {
+    require('glob')(pattern, (err, files) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(files)
+      }
+    })
+  })
+}
 
-Promise.all(argv._.map(pattern => glob(pattern)))
+Promise.all(argv.map(pattern => glob(pattern)))
 .then(lists => lists.reduce((memo, list) => {
   [].push.apply(memo, list)
   return memo
 }, []))
 .catch(err => void console.error('Globbing error:', err))
-.then(files => files.map(file => path.resolve(process.cwd(), file)))
+.then(files => files.map(file => require('path').resolve(process.cwd(), file)))
 .then(files => require('..')({
   files: files
-}).pipe(map(doc => {
-  console.log(JSON.stringify(doc))
-})))
+}).on('data', doc => void console.log(JSON.stringify(doc))))
